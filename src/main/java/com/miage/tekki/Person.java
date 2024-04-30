350f489662763065d9ee77f94c965a54c5f9242b
haimy <h.le@lprs.fr>
2024-04-30 12:12:29 +0200

    modification de la class Person

--- src/main/java/com/miage/tekki/Person.java
+++ src/main/java/com/miage/tekki/Person.java
@@ -3,6 +3,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
-public record Person(String id,String name, String profession, LocalDate birthday,int age, String zodiac, String birthPlace, int heightInCm,String eyeColor, String hairColor) {
+public record Person(String id,String name, String profession, LocalDate birthday,int age, String zodiac, String birthPlace, int heightInCm,String eyeColor, String hairColor, char sexe) {
 
 }
